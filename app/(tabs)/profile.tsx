33665bb96a37df262af60d0ac98a501bd8e73d4c
Shubhranshi Srivastava <shubhranshisrivastava@gmail.com>
2026-07-04 20:42:56 +0530

    Add profile header and user card UI

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,3 +1,4 @@
+import {MaterialIcons} from "@expo/vector-icons";}
 import {
   ScrollView,
   StyleSheet,
@@ -19,6 +20,22 @@ export default function Profile() {
           Manage your account and preferences
         </Text>
     </View>
+    <View style={styles.profileCard}>
+      <View style={styles.avatar}>
+        <MaterialIcons
+        name="person"
+        size={60}
+        color="#2563EB"
+        />
+        </View>
+
+        <Text style={styles.name}>
+       User Name
+        </Text>
+        <Text style={styles.role}>
+       Student
+        </Text>
+      </View>
     </ScrollView>
   );
 }
@@ -39,5 +56,31 @@ const styles = StyleSheet.create({
     marginTop: 8,
     fontSize: 16,
     color: "#6B7280",
+  },
+  profileCard: {
+    backgroundColor: "#FFFFFF",
+    marginHorizontal: 20,
+    marginTop: 25,
+    borderRadius: 20,
+    padding: 25,
+    alignItems: "center",
+    },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: "#DBEAFE",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: "700",
+    marginTop: 18,
+  },
+  role: {
+    fontSize: 16,
+    color: "#6B7280",
+    marginTop: 6,
   },
 });
