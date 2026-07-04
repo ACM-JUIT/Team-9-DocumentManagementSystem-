d7058c4c74c940e2372a4d7f36e756adb29dbb90
Shubhranshi Srivastava <shubhranshisrivastava@gmail.com>
2026-07-04 19:52:13 +0530

    Create basic profile screen layout

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,9 +1,43 @@
-import { Text, View } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function Profile() {
-  return (
-    <View style={{ flex:1, justifyContent:"center", alignItems:"center" }}>
-      <Text>Profile Screen</Text>
+  return(
+  <ScrollView
+  style={styles.container}
+  showsVerticalScrollIndicator={false}
+  >
+    <View style={styles.header}>
+      <Text style={styles.title}>
+        Profile
+        </Text>
+        <Text style={styles.subtitle}>
+          Manage your account and preferences
+        </Text>
     </View>
+    </ScrollView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F7FA",
+  },
+  header: {
+    marginHorizontal: 20,
+    marginTop: 55,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "700",
+  },
+  subtitle: {
+    marginTop: 8,
+    fontSize: 16,
+    color: "#6B7280",
+  },
+});
