7e04da7c0c60ff9ae1f02121d867f3577aac92a1
Shubhranshi Srivastava <shubhranshisrivastava@gmail.com>
2026-07-05 17:04:55 +0530

    Create search page layout and SearchBar component

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,9 +1,45 @@
-import { Text, View } from "react-native";
-
+import SearchBar from "@/components/search/SearchBar";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 export default function Search() {
   return (
-    <View style={{ flex:1, justifyContent:"center", alignItems:"center" }}>
-      <Text>Search Screen</Text>
-    </View>
+    <ScrollView
+      style={styles.container}
+      showsVerticalScrollIndicator={false}
+    >
+      <View style={styles.header}>
+        <Text style={styles.title}>
+          Search
+        </Text>
+        <Text style={styles.subtitle}>
+          Find your files instantly
+        </Text>
+      </View>
+      <SearchBar />
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F7FA",
+  },
+  header: {
+    marginHorizontal: 20,
+    marginTop: 55,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "700",
+  },
+  subtitle: {
+    marginTop: 8,
+    fontSize: 16,
+    color: "#6B7280",
+  },
+});
